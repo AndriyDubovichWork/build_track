e075263877babaaef5ece4e4e887037744309095
Andriy Dubovich <86286088+AndriyDubovichWork@users.noreply.github.com>
2025-05-04 13:18:39 +0300

    GET

--- src/app/api/companies/[id]/route.ts
+++ src/app/api/companies/[id]/route.ts
@@ -2,26 +2,12 @@
 import { getCompanyById } from '@/app/lib/DB/db';
 import { NextRequest, NextResponse } from 'next/server';
 
-export default async function handler(
+// Handle GET request for /api/companies/[id]
+export async function GET(
   request: NextRequest,
   context: { params: { id: string } }
 ) {
-  const method = request.method;
-
-  switch (method) {
-    case 'GET':
-      return handleGetCompany(context.params.id);
-
-    default:
-      return NextResponse.json(
-        { error: `Method ${method} not allowed` },
-        { status: 405 }
-      );
-  }
-}
-
-async function handleGetCompany(id: string) {
-  const companyId = parseInt(id);
+  const companyId = parseInt(context.params.id);
   if (isNaN(companyId)) {
     return NextResponse.json({ error: 'Invalid company ID' }, { status: 400 });
   }
